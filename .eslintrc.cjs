ceebf2af1ce58cce5a91cec2982a440631103386
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2023-07-01 01:22:28 -0400

    Update linter rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,5 +7,7 @@ module.exports = {
         'no-constant-condition': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/no-inferrable-types': 'off',
     },
 };
