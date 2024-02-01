df8ccd3e1c808f014dd89d5bb7f1b47b0864ec77
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2024-02-01 12:53:00 -0500

    New linter rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,9 +5,11 @@ module.exports = {
     root: true,
     rules: {
         'no-constant-condition': 'off',
+        'no-async-promise-executor': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/no-empty-function': 'off',
         '@typescript-eslint/no-non-null-assertion': 'off',
         '@typescript-eslint/no-inferrable-types': 'off',
+        '@typescript-eslint/no-unused-vars': ['warn', { 'argsIgnorePattern': '^_' }]
     },
 };
